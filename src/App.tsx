58091b9b11a222587f2623ff854bfe4dda10d1f1
Sabci0 <barteksabcio@gmail.com>
2025-04-02 17:11:23 +0200

    lesson

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import AgroturismPage from './pages/AgroturismPage';
 import Home from './pages/Home'
 import { Routes, Route } from "react-router";
 import ProductsPage from './pages/ProductsPage';
+import FeathersPage from './pages/FeathersPage';
+import EggsPage from './pages/EggsPage';
+import GalleryPage from './pages/Gallerypage';
 
 function App() {
 
@@ -12,6 +15,9 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/agro" element={<AgroturismPage/>} />
         <Route path="/wyroby" element={<ProductsPage/>} />
+        <Route path="/piora" element={<FeathersPage />} />
+        <Route path="/jajka" element={<EggsPage />} />
+        <Route path="/galeria" element={<GalleryPage />} />
       </Routes>
    </>
   )
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,26 +1,31 @@
 import { Link } from "react-router-dom";
 
- const NavBar = () => { 
+const NavBar = () => { 
   return (
-    <nav className="bg-white p-4 flex justify-between items-center" >
-      
-        <div className="text-2xl font-bold ">Logo</div>
-        
-        <ul className="flex space-x-6">
-          <li>
-            <Link to="/">Home</Link>
-            </li>
-          <li>
-            <Link to="/agro">agro</Link>
-            </li>
-          <li><a href="/">wyroby</a></li>
-          <li><a href="/">piora</a></li>
-          <li><a href="/">jajka</a></li>
-          <li><a href="/">galeria</a></li>
-        </ul>
-      
+    <nav className="bg-white p-4 flex justify-between items-center">
+      <div className="text-2xl font-bold">Logo</div>
+      <ul className="flex space-x-6">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/agro">Agro</Link>
+        </li>
+        <li>
+          <Link to="/wyroby">Wyroby</Link>
+        </li>
+        <li>
+          <Link to="/piora">Piora</Link>
+        </li>
+        <li>
+          <Link to="/jajka">Jajka</Link>
+        </li>
+        <li>
+          <Link to="/galeria">Galeria</Link>
+        </li>
+      </ul>
     </nav>
   );
-}
+};
 
 export default NavBar;
