686a1bbed3e76b83432c5387beea96429b468184
Sabci0 <barteksabcio@gmail.com>
2025-04-09 17:06:20 +0200

    some

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,29 +1,82 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const NavBar = () => { 
+const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
   return (
-    <nav className="bg-white p-4 flex justify-between items-center">
-      <div className="text-2xl font-bold">Logo</div>
-      <ul className="flex space-x-6">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/agro">Agro</Link>
-        </li>
-        <li>
-          <Link to="/wyroby">Wyroby</Link>
-        </li>
-        <li>
-          <Link to="/piora">Piora</Link>
-        </li>
-        <li>
-          <Link to="/jajka">Jajka</Link>
-        </li>
-        <li>
-          <Link to="/galeria">Galeria</Link>
-        </li>
-      </ul>
+    <nav className="bg-white">
+      <div className="flex justify-between items-center p-4 mx-auto max-screen-xl">
+        <div className="flex justify-between items-center w-full">
+          <Link to="/" className="text-2xl font-bold ml-4 text-black  whitespace-nowrap hover:text-yellow-600">
+            Logo
+          </Link>
+        </div>
+        <button
+          className="md:hidden text-black focus:outline-none"
+          onClick={toggleMenu}
+        >
+          {
+            isMenuOpen ? (
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
+                viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            ) : (
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
+                viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+            )}
+        </button>
+
+        <ul className="hidden md:flex space-x-6 whitespace-nowrap">
+          <li>
+            <Link to="/" className="text-black hover:text-yellow-600">Strona Gowna</Link>
+          </li>
+          <li>
+            <Link to="/agro" className="text-black hover:text-yellow-600">Agro</Link>
+          </li>
+          <li>
+            <Link to="/wyroby" className="text-black hover:text-yellow-600">Wyroby</Link>
+          </li>
+          <li>
+            <Link to="/piora" className="text-black hover:text-yellow-600">Piora</Link>
+          </li>
+          <li>
+            <Link to="/jajka" className="text-black hover:text-yellow-600">Jajka</Link>
+          </li>
+          <li>
+            <Link to="/galeria" className="text-black hover:text-yellow-600">Galeria</Link>
+          </li>
+        </ul>
+      </div>
+      {isMenuOpen && (
+        <div className="md:hidden flex justify-end">
+          <ul className=" space-x-6 px-4 space-y-2 whitespace-nowrap">
+            <li>
+              <Link to="/" className="text-black hover:text-yellow-600">Strona Gowna</Link>
+            </li>
+            <li>
+              <Link to="/agro" className="text-black hover:text-yellow-600">Agro</Link>
+            </li>
+            <li>
+              <Link to="/wyroby" className="text-black hover:text-yellow-600">Wyroby</Link>
+            </li>
+            <li>
+              <Link to="/piora" className="text-black hover:text-yellow-600">Piora</Link>
+            </li>
+            <li>
+              <Link to="/jajka" className="text-black hover:text-yellow-600">Jajka</Link>
+            </li>
+            <li>
+              <Link to="/galeria" className="text-black hover:text-yellow-600">Galeria</Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
+import GalleryCarousel from '../components/GalleryCarousel';
 
 const GalleryPage: React.FC = () => {
   return (
     <>
     <NavBar/>
-    <div className="p-4">
-      <h1 className="text-3xl font-bold text-black">Gallery Page</h1>
-      <p className="text-black">View our gallery of farm images.</p>
-    </div>
+    <GalleryCarousel/>
   <Footer/>
     </>
   );
